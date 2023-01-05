db69627ec0f682b7ffaba3d637e8f08714e3bfa4
Mohamed Ali RAIS <mohamed-ali.rais-prestataire@bforbank.com>
2023-01-05 13:48:34 +0100

    fix(monster-back) set business entity ID generation strategy

--- business/src/main/java/com/monster/business/entity/Business.java
+++ business/src/main/java/com/monster/business/entity/Business.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 public class Business {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @Column(name = "name")
     private String name;
